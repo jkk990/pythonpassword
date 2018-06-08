56c5f5666fc7b1afc4410c487d85739123517758
james <kariukikaranja@yahoo.com>
2018-06-08 22:50:15 +0300

    added a new test to test my first method

--- test.py
+++ test.py
@@ -14,3 +14,13 @@ class TestUser(unittest.TestCase):
         Set up method to run before each test cases.
         '''
         self.new_user = User("jk@b.com", "allow")
+
+        def test_init(self):
+            '''
+            test_init test case to test if the object is initialized properly
+            '''
+            self.assertEqual(self.new_user.email,"jk@b.com")
+            self.assertEqual(self.new_user.password,"allow")
+
+if __name__ == '__main__':
+    unittest.main()
