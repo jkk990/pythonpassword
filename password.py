a635e432626bd456dfaae546c9f447e68714e28b
james <kariukikaranja@yahoo.com>
2018-06-12 12:53:52 +0300

    added credentials data to run.py

--- password.py
+++ password.py
@@ -1,5 +1,3 @@
-import sys
-
 class User:
     """
     Class that generates new instances of user data
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3.6
+import sys
 from password import User
 from credential import Credential
 
@@ -20,3 +21,26 @@ def del_user(user):
     Function to delete a user
     '''
     user.delete_user()   
+
+def main():
+        print ('\nPassword Request Program v.01\n')
+        password = id("allow")
+        user_input = input("Please Enter Password: ")
+	
+        if user_input != password:
+            sys.exit("Incorrect Password, terminating... \n")
+		
+            print ("User is logged in!\n")
+
+def create_credential(account_name,account_email,account_password):
+    '''
+    function to create a new credential
+    '''
+    new_credential = Credential(account_name,account_email,account_password)      
+    return new_credential
+
+def save_credential(credential):
+    '''
+    Function to save contact
+    '''
+    credential.save_credential()
