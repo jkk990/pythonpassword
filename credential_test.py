430d5ec82825b9d7902d6ea6cd6446107303e31b
james <kariukikaranja@yahoo.com>
2018-06-12 09:39:08 +0300

    did a display all credential test and failed

--- credential_test.py
+++ credential_test.py
@@ -78,6 +78,13 @@ class TestUser(unittest.TestCase):
 
            self.assertTrue(credential_exists)
 
+    def test_display_all_credential(self):
+           '''
+           method that returns a list of all credentials saved
+           '''
+
+           self.assertEqual(Credential.display_credential(),Credential.credential_list) 
+
           
 if __name__ == "__main__":
     unittest.main()
